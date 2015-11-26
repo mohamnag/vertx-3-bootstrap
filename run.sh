0a1ef0cee1eea3c5357b31cb529ee9d063a6482c
Mohammad Naghavi <mohamnag@gmail.com>
2015-11-26 15:17:14 +0100

    replacing vertx run command with new one that works better

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 gradle clean build copyDeps
-vertx run io.vertx.example.SampleVerticle -cp build/libs/*:build/libs/dependencies/*
+#vertx run io.vertx.example.SampleVerticle -cp build/libs/*:build/libs/dependencies/*
+CLASSPATH="build/libs/*:build/libs/dependencies/*" vertx run io.vertx.example.SampleVerticle
